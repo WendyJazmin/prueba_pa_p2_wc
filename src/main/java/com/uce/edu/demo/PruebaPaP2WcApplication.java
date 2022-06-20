95b643aa4c796c998a0e6834159dc3637b091c23
wendyjazmin <wendyjazmin1994@hotmail.com>
2022-06-20 18:20:37 -0500

    repositorio creado

---
+++ src/main/java/com/uce/edu/demo/PruebaPaP2WcApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaPaP2WcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaPaP2WcApplication.class, args);
+	}
+
+}
